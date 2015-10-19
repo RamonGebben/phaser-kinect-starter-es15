09fa7232e3b80b60f61825e5a7921a54ef2bf3e9
Oz <oscar@touchtribe.nl>
2015-10-19 18:21:07 +0200

    Fullscreen mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import GameState from './states/GameState';
 
 export default class Game extends Phaser.Game {
 	constructor() {
-		super(window.clientHeight, window.clientWidth, Phaser.AUTO, 'phaser-example', null);
+		super(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', null);
 		this.state.add('GameState', GameState, false);
 		this.state.start('GameState');
 	}	
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,20 +1,21 @@
 export default class GameState extends Phaser.State {
 	
 	constructor() {
-		super();	
+		super();
 	}
 	
 	preload() {
 		this.game.load.image('phaser', 'assets/preload.png');
-		Phaser.ScaleManager.EXACT_FIT
 	}
 	
 	create() {
-		this.sprite = this.game.add.sprite(0, 0, 'phaser');
+		this.sprite = this.game.add.sprite(370, 370, 'phaser');
+		this.sprite.anchor.setTo(0.5, 0.5);
 	}
 	
 	update() {
-		console.log('update game here');
+		this.sprite.rotation += .02;
+		console.log('some stupid animation that shows it works');
 	}
 	
 }
