34260a0c06adee98814b8a1d4b7f1dfa9b29a88b
Oz <oscar@touchtribe.nl>
2015-10-19 18:27:29 +0200

    didnt need to exprt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import GameState from './states/GameState';
 
-export default class Game extends Phaser.Game {
+class Game extends Phaser.Game {
 	constructor() {
 		super(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', null);
 		this.state.add('GameState', GameState, false);
