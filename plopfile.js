67f228322479b5a05787da472818f41cd58ff61c
Oz <oscar@touchtribe.nl>
2015-10-19 19:03:58 +0200

    sample plop

---
+++ plopfile.js
@@ -0,0 +1,19 @@
+module.exports = function (plop) {
+    plop.setGenerator('test', {
+        description: 'this is a test',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: function (value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'name is required';
+            }
+        }],
+        actions: [{
+            type: 'add',
+            path: 'folder/{{dashCase name}}.txt',
+            templateFile: 'templates/temp.txt'
+        }]
+    });
+};
