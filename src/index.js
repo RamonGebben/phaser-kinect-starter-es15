165ddb58f25c62e5f0ada8c19b2037e036285819
Oz <oscar@touchtribe.nl>
2015-10-20 20:51:32 +0200

    new index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import GameState from './states/GameState';
 class Game extends Phaser.Game {
 	constructor() {
 		super(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', null);
-		this.state.add('GameStateStart', GameState.StateA, false);
-		this.state.add('GameStatePlaying', GameState.StateB, false);
-		this.state.add('GameStateEnd', GameState.StateC, false);
-		this.state.start('GameStateStart');
+		this.state.add('GameState', GameState, false);
+		this.state.start('GameState');
 	}	
 }
 
