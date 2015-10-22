ac7b224068d530f80932b8db2c24d1f1cf288555
Oz <oscar@touchtribe.nl>
2015-10-22 07:04:41 +0200

    added more body to the game

--- src/states/Start.js
+++ src/states/Start.js
@@ -1,12 +1,34 @@
+// possible states
+// init
+// preload loadUpdate loadRender create update preRender render
+// resize paused resumed pauseUpdate shutdown
+
 export default class Start extends Phaser.State {
 	
 	constructor() {
 		super();
 		this.canvas = document.getElementsByTagName('canvas');
+		document.addEventListener('click', this.stateHandler.bind(this), false);
 	}
 	
 	preload() {
 		this.game.load.image('phaser', 'assets/preload.png');
+	}
+	
+	stateHandler(ev) {
+		if (this.game.paused) {
+			this.resumed()
+		} else {
+			this.paused()
+		} 
+	}
+	
+	paused() {
+		this.game.paused = true;
+	}
+	
+	resumed() {
+		this.game.paused = false;
 	}
 	
 	create() {
