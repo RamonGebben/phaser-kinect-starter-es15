2cf5cf9e79ff448982adb5df5d9715695bad4a1e
Ramon Gebben <ramon@touchtribe.nl>
2015-10-23 19:06:18 +0200

    splash

--- src/states/Start.js
+++ src/states/Start.js
@@ -20,18 +20,22 @@ export default class Start extends Phaser.State {
 	preload() {
 	    this.game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
 	    this.game.load.image('background','assets/bg.png');
-
+		this.game.load.image('splash', 'assets/splash.jpg');
 	}
 
 	create() {
+		const fontStyle = { font: "92px Arial", fill: "#182d3b", wordWrap: true, wordWrapWidth: this.game.world.width, align: "center" };
 	    this.game.stage.backgroundColor = '#182d3b';
-	    this.background = this.game.add.sprite(0, 0, 'background');
+	    this.background = this.game.add.sprite(0, 0, 'splash');
 		this.background.height = this.game.height;
 		this.background.width = this.game.width;
 	    this.button = this.game.add.button(this.game.world.centerX - 95, 400, 'button', this.actionOnClick, this, 2, 1, 0);
 	    this.button.onInputOver.add(this.over, this);
 	    this.button.onInputOut.add(this.out, this);
 	    this.button.onInputUp.add(this.up, this);
+
+		text = this.game.add.text(this.game.world.centerX - 300, 250, 'Rocket Science', fontStyle);
+		text.anchor.set(0.5);
 	}
 
 	up() {
