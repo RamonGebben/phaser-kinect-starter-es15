629d4f893f0a5d3d9a1dc7529cba0cbf4f77cd83
olga <olga@touchtribe.com>
2015-10-23 13:44:28 +0200

    shizzle

--- src/states/Editor.js
+++ src/states/Editor.js
@@ -1,12 +1,10 @@
-import Connection from '../Connection.js';
-
 export default class Editor extends Phaser.State {
 
 	constructor() {
 		super();
 		this.parts = [
 			{
-				title: 'head',
+				title: 'Red ball',
 				type: 'head',
 				image: 'assets/parts/head_1.png'
 			},
@@ -16,8 +14,8 @@ export default class Editor extends Phaser.State {
 				image: 'assets/parts/body_1.png'
 			},
 			{
-				title: 'stablizers',
-				type: 'stablizers',
+				title: 'stabilizers',
+				type: 'stabilizers',
 				image: 'assets/parts/stabilizers_1.png'
 			},
 			{
@@ -26,23 +24,58 @@ export default class Editor extends Phaser.State {
 				image: 'assets/parts/jet_1.png'
 			}
 		]
-		this.connection = Connection;
-	}
-
-	onBodyData(data){
-		// Interact here with data bodies;
-		// console.log('From Editor', data);
 	}
 
 	preload() {
+
+
 		this.parts.forEach((part) => {
-			this.game.load.image(part.title, part.image );
+			console.log(part)
+			this.game.load.image(part.title, part.image);
 		});
+
+		this.game.load.image('outline','assets/outline-rocket.png');
 		this.game.load.image('Space', 'assets/bg.png');
 	}
 
 	create() {
+		//sprites
+		console.log('im on editor');
+		let background = this.game.add.sprite(0,0, 'Space');
+		background.width = this.game.width;
+		background.height = this.game.height;
 
+		this.parts.forEach((part, index) => {
+			switch(part.type) {
+				case 'head':
+					this.game.add.sprite(this.game.world.centerX + 310,10, part.title, part.image);
+					break;
+				case 'body':
+					this.game.add.sprite(this.game.world.centerX + 360, 210, part.title, part.image);
+					break;
+				case 'stabilizers':
+					this.game.add.sprite(this.game.world.centerX + 310, 710, part.title, part.image);
+					break;
+				case 'jet':
+					this.game.add.sprite(this.game.world.centerX + 510, 10, part.title, part.image);
+					break;
+				default :
+					'nothing';
+
+			}
+		});
+		//this.parts.inputEnabled = true;
+		//this.parts.input.enableDrag(false, true);
+
+		this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY - 300, 'outline');
+
+		var marker;
+		marker = this.game.add.graphics();
+		marker.lineStyle(2, 0x00FF00, 1);
+		marker.drawRect(this.game.world.centerX + 300, 0, 170, 200);
+		marker.drawRect(this.game.world.centerX + 470, 0, 160, 200);
+		marker.drawRect(this.game.world.centerX + 300, 200, 330, 500);
+		marker.drawRect(this.game.world.centerX + 300, 700, 330, 250);
 	}
 
 	update() {
