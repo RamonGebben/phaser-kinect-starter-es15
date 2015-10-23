d57940a2a748e0560aa11b8e73942a2fc2e0ae4e
Oz <oscar@touchtribe.nl>
2015-10-23 06:37:25 +0200

    cleaned up start

--- src/states/Start.js
+++ src/states/Start.js
@@ -3,16 +3,20 @@
 // preload loadUpdate loadRender create update preRender render
 // resize paused resumed pauseUpdate shutdown
 
+import Preload from './Preload.js';
+
 export default class Start extends Phaser.State {
 	
 	constructor() {
 		super();
-		this.canvas = document.getElementsByTagName('canvas');
-		document.addEventListener('click', this.stateHandler.bind(this), false);		
+		this.canvas = document.getElementsByTagName('canvas'); 
+		this.sprite = null;
+		document.addEventListener('click', this.stateHandler.bind(this));
 	}
 	
 	preload() {
 		this.game.load.image(this.key, 'assets/preload.png');
+		this.game.load.image('Space', 'assets/bg.png');
 	}
 	
 	stateHandler(ev) {
@@ -30,11 +34,23 @@ export default class Start extends Phaser.State {
 	resumed() {
 		this.game.paused = false;
 	}
-	
+		
 	create() {
-		this.sprite = this.game.add.sprite(this.world.centerX, this.world.centerY, this.key);
+		 //  Create 10 'frames'
+		this.sprite = this.game.add.sprite(this.world.centerX, this.world.centerY, this.key);		
 		this.sprite.anchor.setTo(0.5, 0.5);
+		this.sprite.anchor.set(0.5);
+		this.game.add.tween(this.sprite.scale).to( { x: 0.2, y: 0.2 }, 2000, "Sine.easeInOut", true, 500, -1, true );
+		
 		console.log('State', this.key);
+	}
+	
+	preRender() {
+		
+	}
+	
+	render() {
+		console.log('do something in render if u want');
 	}
 	
 	update() {
